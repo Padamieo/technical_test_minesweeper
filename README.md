82c6908620605d38a37f0fa486abb52ea0f73f4f
Padam <ninjakisses@googlemail.com>
2017-08-14 20:02:52 +0100

    initial development setup and pixi placeholder example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # technical_test_minesweeper
 game development job technical test minesweeper
+
+#### Quick Start - Game
+Finished game is avaliable in the **_build_** folder. _ A server may be needed to view. _
+
+#### Development Requirments
+
+To develop you will need to have the following on your system.
+* Node : [http://nodejs.org](http://nodejs.org)
+* Grunt : [http://gruntjs.com](http://gruntjs.com)
+* [Google Chrome](http://www.google.com/chrome/) or similiar modern browser.
---
+++ gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt){
+
+	require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify:{
+      options:{
+        banner: '/*<%= pkg.name %> V<%= pkg.version %> made on <%= grunt.template.today("yyyy-mm-dd") %>*/\r',
+        mangle: false,
+        beautify: true,
+				sourceMap: true
+      },
+      app:{
+        files:{
+          'build/app.js': [
+            'node_modules/pixi.js/dist/pixi.js',
+						'src/js/index.js'
+          ]
+        }
+      }
+    },
+
+    copy: {
+      build:{
+        files:[{
+          cwd: 'src/',
+          src: ['**', '!**/js/*.js', '!**/*.{jpg,png,less}'],
+          dest: 'build/',
+          nonull: false,
+          expand: true,
+          flatten: false,
+          filter: 'isFile'
+        },]
+      }
+    },
+
+    watch:{
+      options: {
+  		  livereload: 1337,
+        spawn: false
+  		},
+      js:{
+        files: ['src/js/**.js'],
+        tasks: ['uglify:app']
+      },
+      html:{
+        files: ['src/**.html', 'src/**/**.json', 'src/**/**.css'],
+        tasks: ['copy:build']
+      }
+    },
+
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src : 'build/scripts.js'
+				},
+				options: {
+					server: {
+						baseDir: "build"
+					},
+					baseDir: "./build",
+					watchTask: true
+				}
+			}
+		}
+
+
+
+  });
+
+  // our default task, others will come later
+  grunt.registerTask('default', [
+		'copy:build',
+		'uglify',
+		'browserSync',
+    'watch'
+  ]);
+
+};
